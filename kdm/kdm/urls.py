35514dbf473f7f5951253f4d4238068cedba6194
Niklas Sombert <niklas@ytvwld.de>
2016-07-10 21:10:45 +0200

    Serve static files in debug mode.

--- kdm/kdm/urls.py
+++ kdm/kdm/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from memedb import views
 
@@ -22,3 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home')
 ]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
